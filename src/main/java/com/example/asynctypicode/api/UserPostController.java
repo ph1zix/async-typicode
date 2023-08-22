f295b0a9faa088d1780811e7e5c350169b719477
ph1zix <dbforma3000@gmail.com>
2023-08-22 15:00:17 +0200

    use @Value to extract base uri config

--- src/main/java/com/example/asynctypicode/api/UserPostController.java
+++ src/main/java/com/example/asynctypicode/api/UserPostController.java
@@ -4,6 +4,7 @@ import com.example.asynctypicode.domain.Post;
 import com.example.asynctypicode.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -18,17 +19,20 @@ public class UserPostController {
 
     private final WebClient.Builder webClientBuilder;
 
+    @Value("${external.api.base-uri}")
+    private String baseUri;
+
     @GetMapping("/user/{userId}")
     public Mono<UserPostDto> getUserPostBy(@PathVariable Long userId) {
         Mono<User> userMono = webClientBuilder.build()
             .get()
-            .uri("http://jsonplaceholder.typicode.com/users/{userId}", userId)
+            .uri(baseUri + "/users/{userId}", userId)
             .retrieve()
             .bodyToMono(User.class);
 
         Mono<List<Post>> postsMono = webClientBuilder.build()
             .get()
-            .uri("http://jsonplaceholder.typicode.com/posts?userId={userId}", userId)
+            .uri(baseUri + "/posts?userId={userId}", userId)
             .retrieve()
             .bodyToFlux(Post.class)
             .collectList();
